8c79f2106ee592530f0ec3cf833ecd4fde23db1f
Stanislav Spirin <stanislav.spirin@visitech.ru>
2022-09-24 23:16:32 +0300

    fix ip check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -24,6 +24,7 @@ export class AppController {
   @Post()
   async makeVote(@Body() body: { id: number, token: string }, @Req() req: Request, @RealIP() realIp: string) {
     const [headerIp] = req.headers['X-Real-IP'] ?? [];
+    console.log(headerIp, req.headers)
     return await this.appService.makeVote(body.id, body.token, headerIp, realIp);
   }
 }
